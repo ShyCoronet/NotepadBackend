32247e1f784a1dad9190e4da5c0c1b8febf645e6
Maksim Chegulov <mr.chegulov@gmail.com>
2020-08-28 23:03:23 +0300

    Created Json web token service

---
+++ NotepadBackend/JWS/IJwtService.cs
@@ -0,0 +1,10 @@
+﻿using NotepadBackend.Model;
+
+namespace NotepadBackend.JWS
+{
+    public interface IJwtService
+    {
+        public string GenerateAccessToken(User user);
+        public string GenerateRefreshToken();
+    }
+}
---
+++ NotepadBackend/JWS/JwtService.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using NotepadBackend.Model;
+
+namespace NotepadBackend.JWS
+{
+    public class JwtService : IJwtService
+    {
+        private readonly IConfiguration _configuration;
+
+        public JwtService(IConfiguration configuration) =>
+            _configuration = configuration;
+
+        public string GenerateAccessToken(User user)
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            JwtSecurityToken jwt = new JwtSecurityToken(
+                issuer: TokenConfiguration.Issuer,
+                audience: TokenConfiguration.Audience,
+                notBefore: utcNow,
+                claims: GetIdentity(user).Claims,
+                expires: utcNow.Add(TimeSpan.FromMinutes(TokenConfiguration.AccessLifeTime)),
+                signingCredentials: new SigningCredentials(
+                    TokenConfiguration.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            string encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
+        }
+
+        public string GenerateRefreshToken()
+        {
+            DateTime utcNow = DateTime.UtcNow;
+
+            JwtSecurityToken jwt = new JwtSecurityToken(
+                issuer: TokenConfiguration.Issuer,
+                audience: TokenConfiguration.Audience,
+                notBefore: utcNow,
+                expires: utcNow.Add(TimeSpan.FromMinutes(TokenConfiguration.RefreshLifeTime)),
+                signingCredentials: new SigningCredentials(
+                    TokenConfiguration.GetSymmetricSecurityKey(), SecurityAlgorithms.HmacSha256));
+
+            string encodedJwt = new JwtSecurityTokenHandler().WriteToken(jwt);
+
+            return encodedJwt;
+        }
+
+        private ClaimsIdentity GetIdentity(User user)
+        {
+            List<Claim> claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+                new Claim(ClaimTypes.Role, user.Role)
+            };
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "GetToken", ClaimTypes.NameIdentifier,
+                ClaimTypes.Role);
+
+            return claimsIdentity;
+        }
+    }
+}
