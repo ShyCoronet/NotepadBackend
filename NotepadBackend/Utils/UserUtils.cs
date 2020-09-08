d9b9a73c332db1b6e1e6e339d3244cc34707f9a5
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-08 20:48:55 +0300

    Move method from NoteController in separate class

---
+++ NotepadBackend/Utils/UserUtils.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+
+namespace NotepadBackend.Utils
+{
+    public static class UserUtils
+    {
+        public static long GetUserIdFromRequest(HttpContext context)
+        {
+            ClaimsIdentity identity = context.User.Identity as ClaimsIdentity;
+            long userId = long.Parse(identity.Claims.FirstOrDefault(
+                c => c.Type == ClaimTypes.NameIdentifier).Value);
+
+            return userId;
+        }
+    }
+}
