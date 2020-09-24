409f2d0554639f2ce371d6d2684e8375e41f41b6
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-24 17:22:40 +0300

    Added xml comment

--- NotepadBackend/Utils/UserUtils.cs
+++ NotepadBackend/Utils/UserUtils.cs
@@ -6,6 +6,11 @@ namespace NotepadBackend.Utils
 {
     public static class UserUtils
     {
+        /// <summary>
+        /// Returns the user ID from the request context
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
         public static long GetUserIdFromRequest(HttpContext context)
         {
             ClaimsIdentity identity = context.User.Identity as ClaimsIdentity;
