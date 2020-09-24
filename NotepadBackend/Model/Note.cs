4e8c0a53bb6c60078a6f50cf8dd8b5fb6aaa5c23
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-24 17:39:15 +0300

    Added new property CreationDateTimeInSeconds

--- NotepadBackend/Model/Note.cs
+++ NotepadBackend/Model/Note.cs
@@ -15,15 +15,23 @@ namespace NotepadBackend.Model
         public long NoteId { get; set; }
         public string Name { get; set; }
         public DateTime CreationDateTime { get; set; }
+        public long CreationDateTimeInSeconds { get; set; }
         public string Content { get; set; }
+        
+        [JsonIgnore]
         public long UserId { get; set; }
         
         public static Note CreateNote()
         {
+            DateTime now = DateTime.Now;
+            long nowInSeconds = Convert.ToInt64(
+                now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
+            
             Note newNote = new Note
             {
                 Name = "",
-                CreationDateTime = DateTime.Now,
+                CreationDateTime = now,
+                CreationDateTimeInSeconds = nowInSeconds,
                 Content = "",
             };
 
