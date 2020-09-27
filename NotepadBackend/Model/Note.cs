3f6c4ca99a2a78ffc4aad77282c8b471dd2a2323
Maksim Chegulov <mr.chegulov@gmail.com>
2020-09-27 18:05:01 +0300

    Removed CreationDateTimeInSeconds property

--- NotepadBackend/Model/Note.cs
+++ NotepadBackend/Model/Note.cs
@@ -1,12 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
 
 namespace NotepadBackend.Model
 {
@@ -15,7 +9,6 @@ namespace NotepadBackend.Model
         public long NoteId { get; set; }
         public string Name { get; set; }
         public DateTime CreationDateTime { get; set; }
-        public long CreationDateTimeInSeconds { get; set; }
         public string Content { get; set; }
         
         [JsonIgnore]
@@ -23,15 +16,10 @@ namespace NotepadBackend.Model
         
         public static Note CreateNote()
         {
-            DateTime now = DateTime.Now;
-            long nowInSeconds = Convert.ToInt64(
-                now.Subtract(new DateTime(1970, 1, 1)).TotalSeconds);
-            
             Note newNote = new Note
             {
                 Name = "",
-                CreationDateTime = now,
-                CreationDateTimeInSeconds = nowInSeconds,
+                CreationDateTime = DateTime.Now,
                 Content = "",
             };
 
